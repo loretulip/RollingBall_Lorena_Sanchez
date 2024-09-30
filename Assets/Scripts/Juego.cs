495b0786937521706bba071ec16413f7efffc688
loretulip <lorenasvez@gmail.com>
2024-09-30 18:15:25 +0200

    Avance 30/09

---
+++ Assets/Scripts/Juego.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Juego : MonoBehaviour
+{
+    [SerializeField] int vel;
+    [SerializeField] Vector3 dir;
+    float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += 1 * Time.deltaTime;
+        transform.Translate(dir * vel * Time.deltaTime);
+        //Debug.Log(timer);
+        if (timer >= 1)
+        {
+            timer = 0;
+            dir *= -1;
+        }
+       
+        
+
+        //if (direccionR=false)
+        //{
+        //    direccionR=true;
+        //}
+        //else if (direccionR=true)
+        //{
+        //    transform.Translate(-dir * vel * Time.deltaTime);
+        //    direccionR = false;
+        //}
+    }
+
+}
---
+++ Assets/Scripts/Rotacion.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour
+{
+    [SerializeField] Vector3 dir;
+    [SerializeField] int vel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(dir * vel * Time.deltaTime);
+    }
+}
