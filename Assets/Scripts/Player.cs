b146999519245413d25c22a3940ffc4d726ab8c3
loretulip <lorenasvez@gmail.com>
2024-10-03 21:21:22 +0200

    Gravedad 03/10

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody rb;
     [SerializeField] int vel;
-     Vector3 direccionB;
+    [SerializeField] Vector3 direccionB;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h =  Input.GetAxisRaw("Horizontal"); //h= -1 (A, <-) ||| h= 1 (D, ->) ||| h=0 (NADA)
-        float v=  Input.GetAxisRaw("Vertical"); //v= -1 S, abajo ||| v= 1 (W, arriba) ||| v=0 (NADA)
-        direccionB.x = h;
-        direccionB.y = v;
-        transform.Translate(new Vector3(h,0,v).normalized*vel*Time.deltaTime);
+        //float h =  Input.GetAxisRaw("Horizontal"); //h= -1 (A, <-) ||| h= 1 (D, ->) ||| h=0 (NADA)
+        //float v=  Input.GetAxisRaw("Vertical"); //v= -1 S, abajo ||| v= 1 (W, arriba) ||| v=0 (NADA)
+        //direccionB.x = h;
+        //direccionB.y = v;
+        //transform.Translate(new Vector3(h,0,v).normalized*vel*Time.deltaTime);
         //transform.Translate(direccion*velocidad*Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+         GetComponent<Rigidbody>().AddForce(new Vector3(0,5,0), ForceMode.Impulse);
+        }
     }
 }
